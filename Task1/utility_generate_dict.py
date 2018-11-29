257eb1a54e0279951a363ba9e6a11a38cf9aef4f
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-11-29 01:21:21 -0500

    All HTML files parsed and added final json file created

--- Task1/utility_generate_dict.py
+++ Task1/utility_generate_dict.py
@@ -8,11 +8,14 @@ cacm_file_name_2 : parse_tokenized_data_file_2....}
 import json
 import os
 from pathlib import Path
+from bs4 import BeautifulSoup
+import string
+import re
 
 
 def read_json_document(json_file_name):
     """
-    Helper function to read all tyhe path variables form the json file
+    Helper function to read all the path variables form the json file
     Note: We are maintaining a json file to read all the input paths of our
     test collection and also the output_paths
 
@@ -28,7 +31,7 @@ def read_json_document(json_file_name):
     return data
 
 
-def parse_tokenize_store_data(json_file_name):
+def store_data(json_file_name):
     """
     Function that will parse and tokenize all HTML files in the CACM collection
     :param json_file_name: The path to the json file containing all the absolute
@@ -56,14 +59,137 @@ def parse_tokenize_store_data(json_file_name):
     os_specific_path = os.path.realpath("..") + all_paths_dict["test_data"]["test_collection_path"]
     non_dependent_path = Path(os_specific_path)
 
+    # Get the filename where you want to store the parsed and tokenized output
+    parsed_tokenized_output_json_filename = Path(os.path.realpath("..") + all_paths_dict["parsed_tokenized_output_json_file"])
+
+    # Note: The url_text_dict will be of the form
+    # {CACM_file_1 : parsed_tokenized_text_file_1,
+    # CACM_file_2 : parsed_tokenized_text_file_2}
+    url_text_dict = perform_parsing_tokenization(non_dependent_path)
+
+    # Write this dictionary to a json file
+    with open(parsed_tokenized_output_json_filename, "w+") as o_fd:
+        json.dump(url_text_dict, o_fd, indent=4)
 
 
+def perform_parsing_tokenization(folder_path):
+    """
+    Function that will perform the core of the parsing and the tokenization
+    :param folder_path: Path to the folder containing all the .html files
+    :return: a dictionary of the form
+    # {CACM_file_1 : parsed_tokenized_text_file_1,
+    # CACM_file_2 : parsed_tokenized_text_file_2"}
+    """
+
+    # This will be the dictionary that will be returned
+    final_dict = {}
+
+    # Iterate through the folder
+    for html_file in os.listdir(folder_path):
+        if html_file.endswith(".html"):
+            # Just a safety check, we need to parse only HTML files
+            # Get the doc id for this particular HTMl file
+            # for example html_file = "abcd.html"
+            # doc_id = "abcd"
+            doc_id = html_file[:-5]
+
+            # Read the HTML contents of this file
+            with open(folder_path / html_file) as html_fd:
+                raw_html_data = html_fd.read()
+
+            # Now contents contains all the data in the HTMl file
+            # Create BeautifulSoup object
+            temp_bs4_obj = BeautifulSoup(raw_html_data, features="html5lib")
+
+            # First get the title of this page
+            title = temp_bs4_obj.title
+            if title:
+                # If title is present
+                title_text = title.text
+            else:
+                title_text = " "
+
+            non_parsed_text = title_text + " " + temp_bs4_obj.text
+
+            # Finally perform punctuation handling and write this
+            # and store the text in a dictionary
+            final_dict[doc_id] = perform_punctuation_handling(non_parsed_text)
+
+    return final_dict
 
 
+def perform_punctuation_handling(text):
+    """
+    Function that performs punctuation handling of the text present
+    :param text: The dump of text obtained from parsing the HTML
+    (removing unwanted tags)
+    :return: text wherein punctuation is removed except hyphens
+    and punctuation withing digits are maintained
+
+    Examples:
+    Initially
+    text = "abdc.efjhi, lmnoabcd-efgh-jkl122-2345,789123a-bcdABC$$$##, CDEF xyz mnop,,,,"
+    Convert this to a list
+    list_of_text = ["abdc.efjhi, lmno", "abcd-efgh-jkl", "122-2345,789", "123a-bcd", "ABC$$$##, CDEF xyz mnop,,,,"]
+    perform_punctuation_handling(list_of_text)
+
+    The list_of_text now will be as follows:
+    ['abdc efjhi  lmno', 'abcd-efgh-jkl', '122-2345,789', '123a-bcd', 'ABC       CDEF xyz mnop    ']
+    """
+
+    # Convert the given text to list of strings by using the
+    # in-built split method
+    list_of_text = text.split()
+
+    # Create a regular expression compiler
+    # replace all punctuations except hyphens
+    p = re.compile(r"(\b[-]\b)|[\W_]")
+
+    # Create a set of punctuation
+    puncs = set(string.punctuation)
+    for idx, text in enumerate(list_of_text):
+        if check_if_digits_punctuation(text, puncs):
+            # Is a a number with punctuations
+            # Dont do anything
+            continue
+        else:
+            final_text = p.sub(lambda m: (m.group(1) if m.group(1) else " "), text)
+            list_of_text[idx] = final_text
+
+    return strip_additional_spaces(" ".join(list_of_text))
+
+
+def check_if_digits_punctuation(given_str, puncs):
+    """
+    Helper function that checks if a given string only conatisn digits and punctuations
+    :param puncs: set of all punctuation marks in string.punctuation
+    :param given_str: a string
+    :return: True, iff the string contains digits and punctuations
+    """
+
+    # if everything is a punctuation
+    if all(i in string.punctuation for i in given_str):
+        return False
+
+    # Check if the string is composed of only digits and punctuation
+    if all(i.isdigit() or i in puncs for i in given_str):
+        return True
+
+    return False
+
+
+def strip_additional_spaces(text):
+    """
+    Helper function that will strip out additional spaces present in text
+    :param text: The text from the HTML obtained
+    :return:
+    """
+    return re.sub(' +', ' ', text).rstrip().lstrip()
 
 # Store the path of the json file containing the paths
 # of the other required files
 # in a global variable
 
+
 all_paths_json_file_name = "..\\all_paths.json"
-parse_tokenize_store_data(json_file_name=all_paths_json_file_name)
+store_data(json_file_name=all_paths_json_file_name)
